5ed4915a7966acb718255c414f60cd869626d408
Trevor Jung <trevorrjung@gmail.com>
2015-11-25 18:46:49 -0800

    Heroku numeric error

--- db/migrate/20151126023556_change_price_to_integer.rb
+++ db/migrate/20151126023556_change_price_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangePriceToInteger < ActiveRecord::Migration
   def change
-    change_column :products, :price, 'integer USING (trim(price)::integer)'
+    change_column :products, :price, 'numeric USING price::numeric(10,2)'
   end
 end
